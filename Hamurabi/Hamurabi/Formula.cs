e9b238049484fb06cc2fc56f1c7f2eed0af0427d
Viktor Soukup <vitosoukup@gmail.com>
2021-08-17 12:43:22 +0200

    Revert "Revert Hamurabi"
    This reverts commit f54646f79d70983570017ccaf1dabb15e5c48742.

---
+++ Hamurabi/Hamurabi/Formula.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationGame
+{
+    public class Formula
+    {
+        private Func<State, Random, object> func;
+
+        public Formula(Func<State, Random, object> formula)
+        {
+            func = formula;
+        }
+
+        public object Eval(State s, Random rnd)
+        {
+            return func(s, rnd);
+        }
+    }
+}
---
+++ Hamurabi/Hamurabi/State.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationGame
+{
+    public class State
+    {
+        private Dictionary<string, object> data = new Dictionary<string, object>();
+
+        public State()
+        {
+        }
+
+        public State(Tuple<string, object>[] data)
+        {
+            foreach (var entry in data)
+            {
+                this.data[entry.Item1] = entry.Item2;
+            }
+        }
+
+        public object Get(string name, object defaultValue = null)
+        {
+            if (!data.ContainsKey(name)) return defaultValue;
+            return data[name];
+        }
+
+        public int GetInt(string name, int defaultValue = 0)
+        {
+            if (!data.ContainsKey(name)) return defaultValue;
+            return (int)data[name];
+        }
+
+        public double GetDouble(string name, double defaultValue = 0)
+        {
+            if (!data.ContainsKey(name)) return defaultValue;
+            return (double)data[name];
+        }
+
+        public void Set(string name, object value)
+        {
+            data[name] = value;
+        }
+
+        public State Clone()
+        {
+            State result = new State();
+            foreach (string key in data.Keys)
+            {
+                result.data[key] = data[key];
+            }
+            return result;
+        }
+
+    }
+}
